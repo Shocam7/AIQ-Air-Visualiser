d7cfbe38bcc966d775c76f2491110db910bcee82
Shocam7 <87086200+Shocam7@users.noreply.github.com>
2026-02-01 13:46:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import AIQCamera from '@/components/AIQCamera';
-import InfoPanel from '@/components/InfoPanel';
 
 export default function Home() {
   return (
     <main>
       <AIQCamera />
-      <InfoPanel />
     </main>
   );
 }
